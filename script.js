30f1b57821bb6d69fbde40e813e58e03ebcec5b7
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-09-13 15:35:36 +0100

    positioned second player

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ first.innerHTML = `
     <h2 class="jerseynumber">${keeper.jerseyNumber}</h2>
     <h3 class="position">${keeper.position}</h3>
 `
+var rightBack = new Club("De Gea","2","RB")
+console.log(second)
+second.innerHTML = `
+    <img src="/img/back.png" alt="" srcset="" height="100px">
+    <h3 class="playername">${rightBack.playerName}</h3>
+    <h2 class="jerseynumber">${rightBack.jerseyNumber}</h2>
+    <h3 class="position">${rightBack.position}</h3>
+`
